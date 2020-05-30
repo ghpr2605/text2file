689de24f8e35bc1256cf7d0b395caca0a59b0136
pranay <pranayghiya222@gmail.com>
2020-05-30 15:43:29 +0530

    defaulted file extension to .txt

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 window.onload = () => {
   document.getElementById("download").addEventListener("click", (e) => {
     const name = document.getElementById("name").value;
-    const extension = document.getElementById("extension").value;
     const content = document.getElementById("text").value;
-    const filename = `${name}.${extension}`;
-    if (filename && content) {
-      downloadFile(filename, content);
-    }
+    const filename = `${name}.txt`;
+    checkalidation(filename, content);
   });
-};
+}
